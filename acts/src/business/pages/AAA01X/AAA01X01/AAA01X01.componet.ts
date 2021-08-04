980728b0a9ab20593311eede89bab22e937f271d
胡赞來 <361206768@qq.com>
2021-08-04 23:31:41 +0800

    add

--- acts/src/business/pages/AAA01X/AAA01X01/AAA01X01.componet.ts
+++ acts/src/business/pages/AAA01X/AAA01X01/AAA01X01.componet.ts
@@ -14,12 +14,26 @@ import { BusinessUrl } from 'src/business/constant/url-constant';
   templateUrl: './AAA01X01.component.html',
 })
 export class AAA01X01Component implements OnInit {
+  public emailPlaceHolder: string = 'Email';
+  public oldPwdPlaceHolder: string = 'old password';
+  public newPwdPlaceHolder: string = 'new password';
+
   constructor(private router: Router) {}
 
+  /**
+   * Angular life-cycle
+   *
+   * @memberof AAA01X01Component
+   */
   public ngOnInit(): void {
     console.log('successful!');
   }
 
+  /**
+   * 确认
+   *
+   * @memberof AAA01X01Component
+   */
   public confirm(): void {
     this.router.navigate([BusinessUrl.AAA01X02]);
   }

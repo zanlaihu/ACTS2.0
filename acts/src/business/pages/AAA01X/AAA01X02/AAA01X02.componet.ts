78118f9ec669fd0e1ad005ab150ff78a49355daf
胡赞來 <361206768@qq.com>
2021-08-04 15:52:26 +0800

    添加两个新画面

---
+++ acts/src/business/pages/AAA01X/AAA01X02/AAA01X02.componet.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * AAA01X02Component
+ *
+ * @export
+ * @class AAA01X02Component
+ * @implements {OnInit}
+ */
+@Component({
+  selector: 'AAA01X02',
+  templateUrl: './AAA01X02.component.html',
+})
+export class AAA01X02Component implements OnInit {
+  constructor() {}
+
+  public ngOnInit(): void {
+    console.log('successful!');
+  }
+}
---
+++ acts/src/business/pages/AAA01X/AAA01X03/AAA01X03.componet.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * AAA01X03Component
+ *
+ * @export
+ * @class AAA01X03Component
+ * @implements {OnInit}
+ */
+@Component({
+  selector: 'AAA01X03',
+  templateUrl: './AAA01X03.component.html',
+})
+export class AAA01X03Component implements OnInit {
+  constructor() {}
+
+  public ngOnInit(): void {
+    console.log('successful!');
+  }
+}
